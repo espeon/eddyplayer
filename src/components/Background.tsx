8f41d8287ab1a85ed2fa7a0e1bc9376d26384602
Natalie B. <22222885+espeon@users.noreply.github.com>
2026-01-03 21:29:37 -0600

    brighten a bit more

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1177,7 +1177,7 @@ export default function MeshArtBackground({
           stencil: false,
           depth: false,
         }}
-        className="absolute inset-0 brightness-125"
+        className="absolute inset-0 brightness-250"
       >
         <Scene
           imageUrl={currentImageUrl}
